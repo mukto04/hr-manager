1656d0cc979a6c359a2a9054f7abab83dea96690
mukto04 <muktoarifin@gmail.com>
2026-05-12 14:16:18 +0600

    Removed scratch folder to fix build failure

--- scratch/check_data.ts
+++
@@ -1,8 +0,0 @@
-import { masterPrisma } from "../src/lib/prisma";
-
-async function check() {
-  const tenants = await masterPrisma.tenant.findMany();
-  console.log("Tenants in DB:", JSON.stringify(tenants, null, 2));
-}
-
-check();
